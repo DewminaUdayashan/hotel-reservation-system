f1ae2b274e0cd6e3402ea9b71403fc5c35d3f7bf
Dewmina Udayashan <dewminaudayashan@gmail.com>
2025-06-06 21:08:43 +0530

    fix: dis-allow unauthorized users from accessing admin routes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,20 @@
 "use client";
 
+import { useAuth } from "@/hooks/auth/useAuth";
 import DashboardPage from "./dashboard/page";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function AdminDashboard() {
+  const { user, isAdmin } = useAuth();
+
+  useEffect(() => {
+    if (!user || !isAdmin) {
+      redirect("/home");
+    }
+  }, [user, isAdmin]);
+  if (!user || !isAdmin) {
+    return null; // or a loading state
+  }
   return <DashboardPage />;
 }
